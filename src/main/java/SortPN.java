6ded7742f9f5035680072baf3bacd22577ca91a6
Jerrysun <davidsunjie.sun@gmail.com>
2016-05-18 11:48:05 -0700

    improve

--- src/main/java/SortPN.java
+++ src/main/java/SortPN.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Created by <a href="davidsunjie.sun@gmail.com">jerrysun</a> on 5/18/16.
  */
@@ -21,26 +18,21 @@ public class SortPN {
 
     }
 
-    int[] sortArray(int[] arrayContainsPostiveAndNegativeIntegers){
+    public void sortArray(int[] arrayContainsPostiveAndNegativeIntegers){
         this.length = arrayContainsPostiveAndNegativeIntegers.length;
         this.array = arrayContainsPostiveAndNegativeIntegers;
         this.tempMergArr = new int[length];
-        List<Integer> PostiveArr = new ArrayList<Integer>();
-        for(int i =0; i<length; i++){
-            if (array[i]>0){
-                PostiveArr.add(array[i]);
-            }
-        }
+
         doMergeSort(0,length-1);
 
 
-    };
+
+    }
 
     private void doMergeSort(int lowerIndex, int higherIndex) {
 
         if (lowerIndex < higherIndex) {
             int middle = lowerIndex + (higherIndex - lowerIndex) / 2;
-            System.out.print("the midindex is" + middle  + "\n");
             // Below step sorts the left side of the array
             doMergeSort(lowerIndex, middle);
             // Below step sorts the right side of the array
@@ -59,9 +51,9 @@ public class SortPN {
         int j = middle + 1;
         int k = lowerIndex;
 
-        System.out.print("the j is" + j + "\n");
+
         while (i <= middle && j <= higherIndex) {
-            if (tempMergArr[i] <= tempMergArr[j]) {
+            if (tempMergArr[i] >= tempMergArr[j]) {
                 array[k++] = tempMergArr[i++];
 
             } else {
@@ -69,11 +61,9 @@ public class SortPN {
 
             }
         }
-        System.out.print("the  s i is" + i + "\n");
+
 
         while (i <= middle) {
-            System.out.print("the tempMergArr[i] is" + tempMergArr[i] + "\n");
-            System.out.print("the k is" + k + "\n");
             array[k++] = tempMergArr[i++];
 
         }
